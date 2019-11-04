352952ab6197648440c675d7478134b5a70d5232
zhumian <1209320262@qq.com>
2019-11-04 22:52:40 +0800

    1

--- feignservice/src/main/java/com/tuling/feignservice/BaikWarehouseController.java
+++ feignservice/src/main/java/com/tuling/feignservice/BaikWarehouseController.java
@@ -3,16 +3,23 @@ package com.tuling.feignservice;
 
 import com.tuling.dao.BaikWarehouseMapper;
 import com.tuling.entity.BaikWarehouse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/warehouse")
+@Slf4j
 public class BaikWarehouseController {
 
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Resource
     private BaikWarehouseMapper baikWarehouseMapper;
@@ -28,7 +35,8 @@ public class BaikWarehouseController {
 
     @RequestMapping("/getCorrect")
     public BaikWarehouse getCorrect(@RequestParam Integer id){
-        return baikWarehouseMapper.getBaikWarehouseById(id);
+        return new BaikWarehouse();
+//        return baikWarehouseMapper.getBaikWarehouseById(id);
     }
 
 }
--- feignservice/src/main/java/com/tuling/feignservice/FeignserviceApplication.java
+++ feignservice/src/main/java/com/tuling/feignservice/FeignserviceApplication.java
@@ -3,7 +3,10 @@ package com.tuling.feignservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = "com.tuling")
 @EnableEurekaClient
@@ -14,4 +17,9 @@ public class FeignserviceApplication {
         SpringApplication.run(FeignserviceApplication.class, args);
     }
 
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
